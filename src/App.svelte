d9cca694454cbb6d6c62db364b5ced0de1059e93
TimMit <tim.toplak@hotmail.com>
2021-02-24 22:00:45 +0100

    init

---
+++ src/App.svelte
@@ -0,0 +1,23 @@
+<div class="wrapper d-flex justify-content-center align-items-center">
+		<Plane size="60" color="#d1d1d1" unit="px" duration="2s"></Plane>
+		<h1>Website under construction!</h1>
+		<Plane size="60" color="#d1d1d1" unit="px" duration="2s"></Plane>
+</div>
+
+<script>
+	import { Plane } from 'svelte-loading-spinners'
+</script>
+
+<style>
+	.wrapper{
+		background-color: rgb(49, 50, 117);
+		width: 100%;
+		height: 100%;
+		color: #d1d1d1;
+	}
+	h1{
+		margin-left: 34px;
+		margin-right: 14px;
+	}
+	
+</style>
