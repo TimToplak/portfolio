1a70075a40ac5844b6892fb7e20f4e44c1d0b2c9
TimMit <tim.toplak@hotmail.com>
2021-02-26 08:56:56 +0100

    ADD: proof of concept

--- src/App.svelte
+++ src/App.svelte
@@ -1,23 +1,9 @@
-<div class="wrapper d-flex justify-content-center align-items-center">
-		<Plane size="60" color="#d1d1d1" unit="px" duration="2s"></Plane>
-		<h1>Website under construction!</h1>
-		<Plane size="60" color="#d1d1d1" unit="px" duration="2s"></Plane>
-</div>
-
 <script>
-	import { Plane } from 'svelte-loading-spinners'
+  import Home from './Home.svelte';
 </script>
 
+<Home />
+<p>Test</p>
+
 <style>
-	.wrapper{
-		background-color: rgb(49, 50, 117);
-		width: 100%;
-		height: 100%;
-		color: #d1d1d1;
-	}
-	h1{
-		margin-left: 34px;
-		margin-right: 14px;
-	}
-	
 </style>
